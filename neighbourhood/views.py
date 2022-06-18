d54a9668eebb0f52b0b5c739a87879c3311dfb06
edah-hub <edah.chepngetich@student.moringaschool.comm>
2022-06-18 03:49:28 +0300

    adds templates

--- neighbourhood/views.py
+++ neighbourhood/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect,HttpResponseRedirect
+from django.conf import settings
+from django.templatetags.static import static
+from django.http import HttpResponse, Http404
+import datetime as dt
+from .models import *
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
+from .forms import *
+from django.contrib import messages
+
 
 # Create your views here.
+def index(request):
+
+
+    return render(request, 'index.html')
